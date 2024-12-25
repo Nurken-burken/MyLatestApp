cee64843058ea849f8e640bcd640d24749c77bb5
nurken samigullin <samignur@cvut.cz>
2024-12-25 16:43:45 +0100

    push

---
+++ src/main/java/com/example/bankapp/MyLatestAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.bankapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyLatestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyLatestAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/bankapp/MyLatestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.bankapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyLatestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
